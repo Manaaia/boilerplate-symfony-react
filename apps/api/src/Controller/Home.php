ea0311e701fff5efc89501ef97cff8e0ae5cff98
Manon Villard <85735324+Manaaia@users.noreply.github.com>
2025-03-18 11:20:47 +0100

    chore: link front - back (#19)
    - add api endpoint
    - request api endpoint from front
    - display message if request ok

--- apps/api/src/Controller/Home.php
+++ apps/api/src/Controller/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,5 +12,11 @@ final class Home
     public function getWelcome(): Response
     {
         return new Response('Hello World! 🌍<br/> This is the Symfony API and it is up and running as it should. Well done and happy coding!');
+    }
+
+    #[Route('/api', methods: ['GET'])]
+    public function getApi(): JsonResponse
+    {
+        return new JsonResponse(null, Response::HTTP_OK);
     }
 }
